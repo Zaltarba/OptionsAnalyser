ed9409d1787bd69d6627972d6d15b1206fee4866
Zaltarba <122260822+Zaltarba@users.noreply.github.com>
2024-07-13 20:54:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,8 +57,8 @@ except:
     last_price = 1
     
 min_volume = st.number_input('Set minimum volume', value=1000, step=25)
-min_strike = st.slider('Select minimum strike price', 0, 3000, last_price*0.8, 25)
-max_strike = st.slider('Select maximum strike price', 0, 3000, last_price*1.2, 25)
+min_strike = st.slider('Select minimum strike price', 0, 3000, value=last_price*0.8, step=25)
+max_strike = st.slider('Select maximum strike price', 0, 3000, value=last_price*1.2, step=25)
 
 def compute_volatility_surface_plotly(options_data):
     x = options_data['Time to Expiration']
